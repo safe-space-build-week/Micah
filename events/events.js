a47226499da6bef6cc4ee180bcc82d9fac28ec80
micahjones13 <micahjones13@gmail.com>
2019-05-21 16:09:58 -0600

    finished navbar at mobile view. Added nav bar to about page.

--- events/events.js
+++ events/events.js
@@ -1,7 +1,21 @@
 //need click event for button to redirect to signup URL
 
 const singUp = document.querySelector('.sign-up');
-
+const navBar = document.querySelector('.new-header-container');
+const body = document.querySelector('body');
 singUp.addEventListener('click', (event) =>{
     document.location.href= "https://safespace-app.netlify.com/" ;
 });
+
+// add an event to change the mobile view 'new-header-container' to lower opacity on scroll
+
+body.addEventListener('wheel', (event) => {
+    //change the opacity for a limited time
+    navBar.style.opacity = '.5';
+
+    //change it back to solid after a short time
+    setTimeout(function () {
+        navBar.style.opacity = '1';
+    }, 800)
+
+})
