82980967377f3f5c49f94654581108b65c3125e0
micahjones13 <micahjones13@gmail.com>
2019-05-21 13:46:19 -0600

    did some styling, added functionality to sign up button

--- events/events.js
+++ events/events.js
@@ -1,1 +1,7 @@
 //need click event for button to redirect to signup URL
+
+const singUp = document.querySelector('.sign-up');
+
+singUp.addEventListener('click', (event) =>{
+    document.location.href= "https://safespace-app.netlify.com/" ;
+});
